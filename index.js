1b4b8d0f71870e5bfc60919231a70a9a7efa018f
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-06-09 00:12:34 +0530

    CSS changes

--- index.js
+++
@@ -1,9 +0,0 @@
-let apiKey = "d20acb8bb5cec00914bdc9dadae5fcb7"
-async function showWeather(){
-    let city = "Kolkata";
-    let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`)
-    .then(response => response.json());
-    let insertPara = document.createElement('p');
-    insertPara.innerText = data.main.temp;
-    document.body.appendChild(insertPara)
-}
